e2b65802f734c77c3fc5d3e07cf756cad1980d74
AlyonaMarycheva <alyona.marycheva@mail.ru>
2020-04-17 20:26:04 +0300

    added person deletion

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.get('/api/persons/:id', (req, res) => {
     res.status(404).end()
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 
