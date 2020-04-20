e79025b878ed9c0dfac1b400150b76d8912d97b6
AlyonaMarycheva <alyona.marycheva@mail.ru>
2020-04-20 16:09:04 +0300

    adding interface

---
+++ build/precache-manifest.2b096dcdfdda1eb4bd99cd12369647e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141d5d5c14adf44ccb5e434a7d2eacfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b283e3e20323634995fd",
+    "url": "/static/css/main.9847dc05.chunk.css"
+  },
+  {
+    "revision": "9ca361ba87e1be45daad",
+    "url": "/static/js/2.f291ae0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f291ae0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b283e3e20323634995fd",
+    "url": "/static/js/main.3d6ae27b.chunk.js"
+  },
+  {
+    "revision": "4ee863eec7039f899cc1",
+    "url": "/static/js/runtime-main.64cf2cba.js"
+  }
+]);
---
+++ build/precache-manifest.caa04ed2d94d6467b244d0647a936efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbdf3141081882cf7ab9870283edf19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f2d2e52072542da5643",
+    "url": "/static/css/main.9847dc05.chunk.css"
+  },
+  {
+    "revision": "9ca361ba87e1be45daad",
+    "url": "/static/js/2.f291ae0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f291ae0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2d2e52072542da5643",
+    "url": "/static/js/main.b697f131.chunk.js"
+  },
+  {
+    "revision": "4ee863eec7039f899cc1",
+    "url": "/static/js/runtime-main.64cf2cba.js"
+  }
+]);
